26e1d5fd776efef5a5952a6d3edf9da254ed0260
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-13 03:24:31 +0000

    3ero listo

---
+++ emprendedor3.py
@@ -0,0 +1,22 @@
+
+print ("Ingrese el precio de la suscripcion : ")
+p = input()
+p = float(p)
+
+print ("Ingrese número de usuarios normales: ")
+u = input()
+u = float(u)
+
+print ("Ingrese los gastos Totales: ")
+gastos_totales = input()
+gastos_totales = float(gastos_totales)
+
+print ("Ingrese las utilidades del año anterior (debe ser distinto de 0): ")
+u_anterior = input()
+u_anterior = float(u_anterior)
+
+utilidades = p * u - gastos_totales
+
+razon = utilidades/u_anterior
+round(razon, 2)
+print(f"Razón  : {razon} ")
